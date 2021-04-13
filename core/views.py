9eca502355edd87df19d504a9e1bc1538eb2b47d
Juan Farias <juanfarias580@gmail.com>
2021-04-13 18:25:57 -0300

    formatting according to PEP8

--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login as app_login, logout as app_logout
+from django.contrib.auth import (
+                                authenticate,
+                                login as app_login,
+                                logout as app_logout
+                                )
 from django.contrib import messages
 from product.models import Product
 from provider.models import Provider
@@ -12,20 +16,20 @@ from ultils.ultils import currency_format, porcent_format
 
 
 def login(request):
-  return render(request, 'core/login.html')
+    return render(request, 'core/login.html')
 
 
 def verify_login(request):
-  if request.POST:
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
     if user:
-      app_login(request, user)
-      return redirect('index')
+        app_login(request, user)
+        return redirect('index')
     else:
-      messages.error(request, 'Usuário e/ou senha inválidos')
-  return redirect('login')
+        messages.error(request, 'Usuário e/ou senha inválidos')
+    return redirect('login')
 
 
 def logout(request):
@@ -34,78 +38,83 @@ def logout(request):
 
 
 def get_quantity_product_in_stock():
-  entry = int(math.fsum([entry.quantity for entry in StockEntry.objects.all()]))
-  exit = int(math.fsum([exit.quantity for exit in StockExit.objects.all()]))
-  return entry - exit
+    entry = [entry.quantity for entry in StockEntry.objects.all()]
+    exit = [exit.quantity for exit in StockExit.objects.all()]
+    return int(math.fsum(entry)) - int(math.fsum(exit))
 
 
 def dashboard(request):
-  product_quantity = len(Product.objects.all())
-  provider_quantity = len(Provider.objects.all())
-  store_quantity = len(Store.objects.all())
-  qt_product_total_in_stock = get_quantity_product_in_stock()
-  total_revenue = StockExit.get_total_revenue()
-  total_cost = StockEntry.get_total_cost()
-  total_profit = total_revenue - total_cost
-  profit_margin = (total_profit / total_revenue) if total_revenue != 0 else 0
-  return render(request, 'core/index.html',
-    {
-      'product_quantity':product_quantity,
-      'provider_quantity':provider_quantity,
-      'store_quantity':store_quantity,
-      'qt_product_total_in_stock':qt_product_total_in_stock,
-      'total_revenue':currency_format(total_revenue),
-      'total_cost':currency_format(total_cost),
-      'total_profit':currency_format(total_profit),
-      'profit_margin':porcent_format(profit_margin)
-    }
-  )
+    product_quantity = len(Product.objects.all())
+    provider_quantity = len(Provider.objects.all())
+    store_quantity = len(Store.objects.all())
+    qt_product_total_in_stock = get_quantity_product_in_stock()
+    total_revenue = StockExit.get_total_revenue()
+    total_cost = StockEntry.get_total_cost()
+    total_profit = total_revenue - total_cost
+    profit_margin = (total_profit / total_revenue) if total_revenue != 0 else 0
+    return render(request, 'core/index.html', {
+          'product_quantity': product_quantity,
+          'provider_quantity': provider_quantity,
+          'store_quantity': store_quantity,
+          'qt_product_total_in_stock': qt_product_total_in_stock,
+          'total_revenue': currency_format(total_revenue),
+          'total_cost': currency_format(total_cost),
+          'total_profit': currency_format(total_profit),
+          'profit_margin': porcent_format(profit_margin)
+        }
+    )
 
 
 def inventory(request):
-  return render(request, 'core/inventory.html')
+    return render(request, 'core/inventory.html')
 
 
 def get_quantity_product_in_stock_by_id(id):
-  return StockEntry.get_number_of_product_entries(id) - StockExit.get_number_of_product_exit(id)
+    quantity_entry = StockEntry.get_number_of_product_entries(id)
+    quantity_exit = StockExit.get_number_of_product_exit(id)
+    return quantity_entry - quantity_exit
 
 
 def get_total_profit_by_id(id):
-  return StockExit.get_total_revenue_by_id(id) - StockEntry.get_total_cost_by_id(id)
+    total_revenue = StockExit.get_total_revenue_by_id(id)
+    total_cost = StockEntry.get_total_cost_by_id(id)
+    return total_profit - total_cost
 
 
 def get_status(id):
-  qt_product_in_stock = get_quantity_product_in_stock_by_id(id)
-  product = Product.objects.get(id=id)
-  qt_min = product.level_minimum
-  qt_max = product.level_maximum
-  if qt_product_in_stock == 0:
-    return '<p class="bg-danger text-white rounded">Sem Estoque</p>'
-  elif qt_product_in_stock < qt_min or qt_product_in_stock > qt_max:
-    return '<p class="bg-info text-white rounded">Estoque Perigoso</p>'
-  else:
-    return '<p class="bg-success text-white rounded">Estoque Confortável</p>'
+    qt_product_in_stock = get_quantity_product_in_stock_by_id(id)
+    product = Product.objects.get(id=id)
+    qt_min = product.level_minimum
+    qt_max = product.level_maximum
+    if qt_product_in_stock == 0:
+        el = '<p class="bg-danger text-white rounded">Sem Estoque</p>'
+    elif qt_product_in_stock < qt_min or qt_product_in_stock > qt_max:
+        el = '<p class="bg-info text-white rounded">Estoque Perigoso</p>'
+    else:
+        el = '<p class="bg-success text-white rounded">Estoque Confortável</p>'
+    return el
 
 
 def get_data_inventory(request):
-  products = Product.objects.all()
-  response = {"data":
-    [
-      {
-        "DT_RowId":f"row_{product.id}",
-        "Produto":product.item,
-        "Entradas":StockEntry.get_number_of_product_entries(product.id),
-        "Saídas":StockExit.get_number_of_product_exit(product.id),
-        "Estoque Atual":get_quantity_product_in_stock_by_id(product.id),
-        "Estoque Mínimo":product.level_minimum,
-        "Estoque Máximo":product.level_maximum,
-        "Status":get_status(product.id),
-        "Receita Total":currency_format(StockExit.get_total_revenue_by_id(product.id)),
-        "Custo Total":currency_format(StockEntry.get_total_cost_by_id(product.id)),
-        "Lucro":currency_format(get_total_profit_by_id(product.id))
-      }
+    products = Product.objects.all()
+    response = {"data": [
+        {
+            "DT_RowId": f"row_{product.id}",
+            "Produto": product.item,
+            "Entradas": StockEntry.get_number_of_product_entries(product.id),
+            "Saídas": StockExit.get_number_of_product_exit(product.id),
+            "Estoque Atual": get_quantity_product_in_stock_by_id(product.id),
+            "Estoque Mínimo": product.level_minimum,
+            "Estoque Máximo": product.level_maximum,
+            "Status": get_status(product.id),
+            "Receita Total":
+            currency_format(StockExit.get_total_revenue_by_id(product.id)),
+            "Custo Total":
+            currency_format(StockEntry.get_total_cost_by_id(product.id)),
+            "Lucro": currency_format(get_total_profit_by_id(product.id))
+        }
 
-      for product in products
-    ]
-  }
-  return JsonResponse(response)
+        for product in products
+        ]
+    }
+    return JsonResponse(response)
