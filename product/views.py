583678bf3f5e32c3e9ef7650c78a284994d69901
Juan Farias <juanfarias580@gmail.com>
2021-04-13 19:07:20 -0300

    formatting according to PEP8

--- product/views.py
+++ product/views.py
@@ -8,63 +8,59 @@ from django.contrib import messages
 
 
 class ProductListView(ListView):
-  def post(self, request):
-    products = Product.objects.all()
-    response = {'data':
-      [
-        {
-          "DT_RowId":f"row_{product.id}",
-          "Item":product.get_link_update(),
-          "Código do Produto":product.code,
-          "Unidade de Medida":product.unit_of_measure,
-          "Estoque Mínimo":product.level_minimum,
-          "Estoque Máximo":product.level_maximum,
-          "":product.get_button_delete()
-        }
+    def post(self, request):
+        products = Product.objects.all()
+        response = {'data': [
+            {
+              "DT_RowId": f"row_{product.id}",
+              "Item": product.get_link_update(),
+              "Código do Produto": product.code,
+              "Unidade de Medida": product.unit_of_measure,
+              "Estoque Mínimo": product.level_minimum,
+              "Estoque Máximo": product.level_maximum,
+              "": product.get_button_delete()
+            }
 
-        for product in products
-      ]
-    }
-    return JsonResponse(response)
+            for product in products
+            ]
+        }
+        return JsonResponse(response)
 
 
 class ProductCreateView(CreateView):
-  template_name = 'product/form.html'
-  form_class = ProductForm
+    template_name = 'product/form.html'
+    form_class = ProductForm
 
+    def form_valid(self, form):
+        return super().form_valid(form)
 
-  def form_valid(self, form):
-    return super().form_valid(form)
-
-
-  def get_success_url(self):
-    messages.add_message(self.request, messages.SUCCESS, "Produto cadastrado com sucesso !")
-    return reverse('product:main')
+    def get_success_url(self):
+        message = "Produto cadastrado com sucesso !"
+        messages.add_message(self.request, messages.SUCCESS, message)
+        return reverse('product:main')
 
 
 class ProductUpdateView(UpdateView):
-  template_name = 'product/form.html'
-  form_class = ProductForm
+    template_name = 'product/form.html'
+    form_class = ProductForm
 
+    def get_object(self):
+        id = self.kwargs.get('id')
+        return get_object_or_404(Product, id=id)
 
-  def get_object(self):
-    id = self.kwargs.get('id')
-    return get_object_or_404(Product, id=id)
+    def form_valid(self, form):
+        return super().form_valid(form)
 
-
-  def form_valid(self, form):
-    return super().form_valid(form)
-
-
-  def get_success_url(self):
-    messages.add_message(self.request, messages.SUCCESS, "Produto atualizado com sucesso !")
-    return reverse('product:main')
+    def get_success_url(self):
+        message = "Produto atualizado com sucesso !"
+        messages.add_message(self.request, messages.SUCCESS, message)
+        return reverse('product:main')
 
 
 class ProductDeleteView(DeleteView):
-  def post(self, request):
-    id = request.POST.get('id')
-    product = Product.objects.get(id=id)
-    product.delete()
-    response = {'message':'Produto deletado com sucesso !'}
-    return JsonResponse(response)
+    def post(self, request):
+        id = request.POST.get('id')
+        product = Product.objects.get(id=id)
+        product.delete()
+        response = {'message': 'Produto deletado com sucesso !'}
+        return JsonResponse(response)
